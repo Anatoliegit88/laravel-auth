fa2c2fe7ff15a5d2189f496cc310fba31a9269ea
Anatoliegit88 <anatolie.coroban88@gmail.com>
2023-01-10 18:31:30 +0100

    upgrade

--- routes/web.php
+++ routes/web.php
@@ -18,9 +18,13 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::middleware(['auth', 'verified'])
+    ->prefix('admin')
+    ->name('admin.')
+    ->group(function () {
+        Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+        Route::resource('projects', ProjectController::class)->parameters(['projects' => 'projects:slug']);
+    });
 
 require __DIR__.'/auth.php';
