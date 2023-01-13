35e834d97d08c2fd216fd2fafe9ff8cd934c5983
Anatoliegit88 <anatolie.coroban88@gmail.com>
2023-01-13 18:13:46 +0100

    CRUD DONE

--- app/Http/Requests/StoreProjectRequest.php
+++ app/Http/Requests/StoreProjectRequest.php
@@ -13,7 +13,7 @@ class StoreProjectRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +24,13 @@ class StoreProjectRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'title' => ['required', 'max:150', 'unique:projects'],
+            'content' => ['nullable']
         ];
+    }
+
+    public function messages()
+    {
+        return ['title.required' => 'The Title is request'];
     }
 }
